5f9e346aea947a9772359515f5dcd3bc901dfcc3
davidbras63 <bdavid0801@gmail.com>
2026-07-20 00:05:05 +0200

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,10 @@ export default clerkMiddleware(async (auth, req) => {
 });
 
 export const config = {
-  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)', '/(api|trpc)(.*)'],
+  matcher: [
+    // Exclut les fichiers statiques et les images de la vérification middleware
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/(api|trpc)(.*)',
+  ],
 };
+
