9713d662ad438f9d0faf21512a9c018660f6ce37
davidbras63 <bdavid0801@gmail.com>
2026-07-19 02:27:21 +0200

    Add files via upload

--- components/SidebarWrapper.tsx
+++ components/SidebarWrapper.tsx
@@ -1,6 +1,17 @@
 'use client';
+import { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 
 export default function SidebarWrapper() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null; // Le build serveur verra juste "rien", donc pas d'erreur
+  }
+
   return <Sidebar />;
 }
