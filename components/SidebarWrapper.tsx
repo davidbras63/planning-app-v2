78c646e6b6d0966d6b63403f97104fe610f12e48
davidbras63 <bdavid0801@gmail.com>
2026-07-23 00:07:05 +0200

    Delete components directory

--- components/SidebarWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import Sidebar from './Sidebar';
-
-export default function SidebarWrapper() {
-  // On affiche le composant en dur, sans condition de montage
-  return <Sidebar />;
-}
