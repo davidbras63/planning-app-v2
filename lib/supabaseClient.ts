0175208433685b7aa57d3747339e31a724d2bb70
davidbras63 <bdavid0801@gmail.com>
2026-07-22 21:59:58 +0200

    Delete lib directory

--- lib/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// On privilégie la clé service_role en priorité pour le serveur
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
