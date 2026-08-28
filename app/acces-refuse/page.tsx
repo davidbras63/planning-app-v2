209092a5d708683f3c293bfe2b7e3fb8f44b8014
davidbras63 <bdavid0801@gmail.com>
2026-08-28 22:23:37 +0200

    Delete app directory

--- app/acces-refuse/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AccesRefuse() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>Accès restreint</h1>
-      <p>Vous devez être connecté pour accéder à cette partie de l'application.</p>
-      <a href="/">Cliquez ici pour vous connecter</a>
-    </div>
-  );
-}
--- app/subscription/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function SubscriptionPage() {
-  return (
-    <div style={{ padding: '50px', textAlign: 'center' }}>
-      <h1>Période d'essai terminée</h1>
-      <p>Merci d'avoir utilisé notre application. Veuillez souscrire à un abonnement pour continuer.</p>
-      {/* Insère ton bouton de paiement ici */}
-    </div>
-  );
-}
